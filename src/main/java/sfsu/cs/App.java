45d7466ac4445d3a37b2b4a182129bacb9b8c999
Rajani Maski <rajani.maski@gmail.com>
2017-04-23 08:46:12 -0700

    first commit

---
+++ src/main/java/sfsu/cs/App.java
@@ -0,0 +1,13 @@
+package sfsu.cs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
